5211531760ec9e1a43ca59acce359c15c36c9e9f
Jods666 <jods2@sibnet.ru>
2016-03-29 21:52:54 +0600

    wazwww

---
+++ index.php
@@ -0,0 +1,127 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<meta charset="utf-8" />
+	<meta name="description" content="Дескрипция" type="text" />
+	<link rel="stylesheet" type="text/css" href="css/style.css"/>
+	<title>Большой проект</title>
+	<script type="text/javascript" src="http://ajax.googleapis.com/ajax/libs/jquery/1.9.0/jquery.min.js"></script>
+	<script src="js/jquery.flexslider-min.js"></script>
+	<script src="js/slider.js"></script>
+</head>
+<body>
+	<div class="wrapper">
+		<header>
+			<h1><a href="/"><img src="images/logo.png" alt="Логотип"/></a></h1>
+			<span class="tel">123-456-7890</span>
+			<nav>
+				<ul>
+					<li><a href="contact.html" title="Contact">contact</a></li>
+					<li><a href="portfolio.html" title="Portfolio">portfolio</a></li>
+					<li><a href="services.html" title="Services">services</a></li>
+					<li><a href="about.html" title="About">about</a></li>
+					<li class="current"><a href="/" title="Home">home</a></li>
+				</ul> 
+			</nav>
+		</header>
+		<div class="slider-block">
+			<div class="flexslider">
+				<ul class="slides">
+					<li><img src="images/car1.jpg" alt="Машина"/></li>
+					<li><img src="images/car2.jpeg" alt="Машина"/></li>
+					<li><img src="images/car3.jpg" alt="Машина"/></li>
+					<li><img src="images/car4.jpeg" alt="Машина"/></li>
+				</ul>
+			</div>
+			<p>We have what it takes to <br />
+take your website to the</p>
+			<p class="fs35">new height of success</p>
+			<a href="/" class="started">get started here</a>
+		</div>
+		<div class="preim">
+			<ul>
+				<li class="money"><h3>Save big with us</h3>Lorem ipsum dolor sit amet, <br /> 
+consectetur adipiscing elit.</li>
+				<li class="clock"><h3>Always on time</h3>Lorem ipsum dolor sit amet, <br /> 
+consectetur adipiscing elit.</li>
+				<li class="contact"><h3>Contact Us</h3>Lorem ipsum dolor sit amet, <br /> 
+consectetur adipiscing elit.</li>
+			</ul>
+		</div>
+		<div class="content">
+			<ul>
+				<li>
+					<h2>Who we are?</h2>
+					<img src="images/whoweare.gif" alt="Изображение"/>
+					<p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Etiam tristique tristique lacus in varius. 
+					Donec nec vestibulum ligula. Aenean turpis diam, feugiat a luctus in, rhoncus eget risus. 
+					Maecenas consequat porttitor dui, vitae consequat massa imperdiet ut. elit ut tempus lobortis, 
+					eros leo molestie velit, non egestas augue nulla eget erat.</p>
+					<a href="/" class="read_more">read more</a>
+				</li>
+				<li>
+					<h2>What we do?</h2>
+					<img src="images/whatwedo.gif" alt="Изображение"/>
+					<p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Etiam tristique tristique lacus in 
+					varius. Donec nec vestibulum ligula. Aenean turpis diam, feugiat a luctus in, rhoncus eget risus.
+					Maecenas consequat porttitor dui, vitae consequat massa imperdiet ut.</p>
+					<ul class="design">
+						<li><a href="/" title="link">Website Design</a></li>
+						<li><a href="/" title="link">Blog Desing</a></li>
+						<li><a href="/" title="link">Branding Design</a></li>
+					</ul>
+				</li>
+				<li>
+					<h2>Why hire us?</h2>
+					<img src="images/whyhireus.gif" alt="Изображение"/>
+					<p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Etiam tristique tristique lacus in varius. 
+					Donec nec vestibulum ligula. Aenean turpis diam, feugiat a luctus in, rhoncus eget risus. Maecenas 
+					consequat porttitor dui, vitae consequat massa imperdiet ut. Donec laoreet, elit ut tempus lobortis, 
+					eros leo molestie velit, non egestas augue nulla eget erat.</p>
+					<a href="/" class="read_more">read more</a>
+				</li>
+			</ul>
+		</div>
+		<footer>
+		<ul class="main">
+			<li class="BrowseSite">
+				<h3>Browse Site</h3>
+					<ul>
+						<li><a href="#" title="Переход">About our company</a></li>
+						<li><a href="#" title="Переход">Contact us</a></li>
+						<li><a href="#" title="Переход">Our Portfolio</a></li>
+						<li><a href="#" title="Переход">Our Prices</a></li>
+						<li><a href="#" title="Переход">Online quote form</a></li>
+						<li><a href="#" title="Переход">Client Testmonials</a></li>
+					</ul>
+			</li>
+			<li class="OurTestmonials">
+				<h3>Our Testmonials</h3>
+				<p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Etiam tristique tristique lacus in varius. 
+				Donec nec vestibulum ligula. Aenean turpis diam, feugiat a luctus in, rhoncus eget risus. 
+				Maecenas consequat porttitor dui, vitae consequat massa imperdiet ut. elit ut tempus lobortis, 
+				eros leo molestie velit.<br /><span class="bold">Rafi, GraphicsFuel.com</span></p>
+			</li>
+			<li class="Blog">
+				<h3 id="blog">From Our Blog</h3>
+				<p class="date">July 6, 2010</p>
+				<p><span class="blue">Lorem ipsum dolor sit amet, consectetur</span><br /> adipiscing elit. 
+				Etiam tristique tristique lacus in varius. Donec nec vestibulum ligula. Aenean turpis diam...</p><br />
+				<p class="date">July 6, 2010</p>
+				<p><span class="blue">Lorem ipsum dolor sit amet, consectetur</span><br /> adipiscing elit. 
+				Etiam tristique tristique lacus in varius. Donec nec vestibulum ligula. Aenean turpis diam...</p>
+			</li>
+		</ul>
+		<div class="footer_bottom">
+			<p>Copyright © 2010 Business. All rights reserved. Designed by GraphicFuel.com</p>
+			<ul>
+				<li><a href="http://twitter.com"><img src="images/twitter.png" alt="Twitter"/></a></li>
+				<li><a href="http://linkedin.com"><img src="images/linkedin.png" alt="Linkedin"/></a></li>
+				<li><a href="http://facebook.com"><img src="images/facebook.png" alt="Facebook"/></a></li>
+				<li class="logogray"><a href="/"><img src="images/logogray.png" alt="logo"/></a></li>
+			</ul>
+		</div>
+	</footer>
+	</div>
+</body>
+</html>
